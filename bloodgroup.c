21c37a5e5010f8dab458e3b4e5146309c3bbebeb
tartungkam <phubordee.tkntar@mail.kmutt.ac.th>
2021-09-16 17:08:42 +0700

    UserA Modified

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[50], G[5], answer[20];
+{ char Name[50], G[5], answer[40];
   int i;
   do
   {
@@ -43,6 +43,6 @@ void main()
     }
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
-  } while (strcmp(answer, "YES") == 0);
+  } while (strcmp(answer, "yes") == 0);
   printf("Goodbye\n");
 }
